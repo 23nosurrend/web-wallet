95e4fe7dd56d31bb40a9bd0d6ff7fc8c7c8b29fc
23nosurrend <keynesbizimana@gmail.com>
2025-01-14 22:43:33 +0100

    Initial setup and Home Page

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { BrowserRouter as Router } from "react-router-dom";
+// import Routes from "./componet/route/route";
+
+import Routes from "./componet/routes";
+
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
